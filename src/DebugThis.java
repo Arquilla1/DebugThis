c651b2ee383589241b995c19c1b39d0d38f2f1be
Arquilla1 <Arquilla1@msn.com>
2020-04-17 04:36:00 -0400

    first commit

---
+++ src/DebugThis.java
@@ -0,0 +1,34 @@
+/*
+
+
+ */
+
+
+import java.util.Arrays;
+public class DebugThis {
+
+
+    public static class Fix {
+        public static void main(String[] args) {
+
+
+            ////1.Create an array to hold the numbers 1-10.
+            char[] numList = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9',};
+
+            int i;
+            for (i = 0; i < 10; i++) {
+                System.out.println(numList);
+            }
+
+
+////2.Loop through that array and sum the numbers 1-10, then print that total to the screen.
+            char[] newnumList = new char[]{'1', '2', '3', '4', '5', '6', '7', '8', '9',};
+                        int total = 0;
+            for (int index = 0; i < 10; i++) {
+
+
+            System.out.println("The total of the numbers is " + total + numList);
+                {
+        }
+
+    }}}}
